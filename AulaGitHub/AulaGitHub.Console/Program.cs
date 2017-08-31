75a065316e8e90ba934134ddbd34176ff12bc0c5
Ricardo <Ricardo@RICARDO-PC>
2017-08-31 10:50:15 -0300

    Create Console

---
+++ AulaGitHub/AulaGitHub.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AulaGitHub.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Olá meus amiguinhos...");
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
